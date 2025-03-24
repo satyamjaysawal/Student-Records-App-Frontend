385a974bfda705777798d93120ddf162dc9a6f67
Satyam_jaysawal <sjrecm9258@gmail.com>
2025-03-24 21:56:48 +0530

    up

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// const BACKEND_API_BASE_URL = 'https://student-records-app-backend.vercel.app';
-const BACKEND_API_BASE_URL = 'http://localhost:5000';
+const BACKEND_API_BASE_URL = 'https://student-records-app-backend.vercel.app';
+// const BACKEND_API_BASE_URL = 'http://localhost:5000';
